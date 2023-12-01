cfd6f5bce7afbdb9921768a4d098e3acd6e9b137
eldad-assaf <eldadux@gmail.com>
2023-12-01 17:55:25 +0200

    ReusableTextWithAutoSize

--- lib/components/reuseable_text.dart
+++ lib/components/reuseable_text.dart
@@ -1,4 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ReusableText extends StatelessWidget {
   final String text;
@@ -14,6 +17,28 @@ class ReusableText extends StatelessWidget {
       softWrap: false,
       overflow: TextOverflow.fade,
       style: style,
+    );
+  }
+}
+
+class ReusableTextWithAutoSize extends StatelessWidget {
+  final String text;
+  final int? maxLines;
+  final double minFontSize;
+  const ReusableTextWithAutoSize({
+    super.key,
+    required this.text,
+    required this.maxLines,
+    required this.minFontSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoSizeText(
+      text,
+      minFontSize: minFontSize,
+      maxLines: maxLines,
+      overflow: TextOverflow.ellipsis,
     );
   }
 }
--- lib/features/map_view/presentation/pages/home/map_view.dart
+++ lib/features/map_view/presentation/pages/home/map_view.dart
@@ -4,13 +4,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:weather_app/components/reuseable_text.dart';
+import 'package:weather_app/components/text_style.dart';
 import 'package:weather_app/features/device_position/presentation/bloc/device_position_bloc.dart';
 import 'package:weather_app/features/map_view/presentation/bloc/camera_position_bloc.dart';
 import 'package:weather_app/features/map_view/presentation/widgets/weather_modal_sheet.dart';
 import 'package:weather_app/features/realtime_weather/presentation/bloc/realtime_weather_bloc.dart';
 import 'package:weather_app/features/realtime_weather/presentation/bloc/realtime_weather_event.dart';
 import 'package:weather_app/features/realtime_weather/presentation/bloc/realtime_weather_state.dart';
-import 'package:weather_app/features/realtime_weather/presentation/pages/home/realtime_weather.dart';
 import 'package:weather_app/main.dart';
 
 class MapView extends StatefulWidget {
@@ -27,64 +28,55 @@ class MapViewState extends State<MapView> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        bottomNavigationBar: const SizedBox(
+          height: 150,
+          child: ReusableTextWithAutoSize(
+            text:
+                'Click the icon to see weather details for the location shown in the map.',
+            maxLines: 3,
+            minFontSize: 16,
+          ),
+        ),
         body: Stack(children: [
-          BlocConsumer<DevicePositionBloc, DevicePositionState>(
-              listener: (context, state) {
-            print('DevicePositionBloc listener state : $state');
-            if (state is DevicePositionDone) {
-              context
-                  .read<CameraPositionBloc>()
-                  .add(DetermineCameraPositionEvent(state.position));
-            }
-          }, builder: (context, state) {
-            print('DevicePositionBloc builder state : $state');
+          BlocBuilder<CameraPositionBloc, CameraPositionState>(
+            builder: (context, state) {
+              if (state is CameraPositionInitial) {
+                //should not happen
+                return const Center(child: Text('CameraPositionInitial'));
+              }
+              if (state is CameraPositionLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (state is CameraPositionError) {
+                return Center(
+                    child:
+                        Text('CameraPositionError ${state.error.toString()}'));
+              } else if (state is CameraPositionDone) {
+                return GoogleMap(
+                  mapType: MapType.hybrid,
+                  initialCameraPosition: state.cameraPosition!,
+                  onMapCreated: (GoogleMapController controller) async {
+                    _mapController = controller;
+                    _goToThePlace(cameraPosition: state.cameraPosition!);
+                  },
+                  myLocationButtonEnabled: true,
+                  myLocationEnabled: true,
+                  onCameraIdle: () async {
+                    middleOfTheMap = await _mapController!
+                        .getLatLng(const ScreenCoordinate(x: 0, y: 0));
+                  },
+                  onCameraMove: (cameraPosition) {},
+                );
+              } else {
+                return Container(
+                  color: Colors.red,
+                );
+              }
+            },
+          ),
 
-            if (state is DevicePositionLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return BlocBuilder<CameraPositionBloc, CameraPositionState>(
-              builder: (context, state) {
-                if (state is CameraPositionInitial) {
-                  //should not happen
-                  return const Center(child: Text('CameraPositionInitial'));
-                }
-                if (state is CameraPositionLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (state is CameraPositionError) {
-                  return Center(
-                      child: Text(
-                          'CameraPositionError ${state.error.toString()}'));
-                } else if (state is CameraPositionDone) {
-                  return GoogleMap(
-                    mapType: MapType.hybrid,
-                    initialCameraPosition: state.cameraPosition!,
-                    onMapCreated: (GoogleMapController controller) async {
-                      _mapController = controller;
-                      _goToThePlace(cameraPosition: state.cameraPosition!);
-                    },
-                    myLocationButtonEnabled: true,
-                    myLocationEnabled: true,
-                    onCameraIdle: () async {
-                      middleOfTheMap = await _mapController!
-                          .getLatLng(const ScreenCoordinate(x: 0, y: 0));
-
-                      print('position of the middle : $middleOfTheMap');
-                    },
-                  );
-                } else {
-                  return Container(
-                    color: Colors.red,
-                  );
-                }
-              },
-            );
-          }),
           Positioned(
             top: 16,
             left: 16,
@@ -119,39 +111,39 @@ class MapViewState extends State<MapView> {
           //   ),
           // ),
         ]),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () async {
-            context
-                .read<RealtimeWeatherBloc>()
-                .add(FetchRealtimeWeatherEvent(middleOfTheMap!.asString()));
-            await showModalBottomSheet(
-                useSafeArea: true,
-                //barrierColor: Colors.blue,
-                isScrollControlled: true,
-                showDragHandle: true,
-                context: context,
-                builder: (context) {
-                  return BlocBuilder<RealtimeWeatherBloc, RealtimeWeatherState>(
-                    builder: (context, state) {
-                      if (state is RealtimeWeatherLoading) {
-                        return Container();
-                      } else if (state is RealtimeWeatherDone) {
-                        return WeatherModalSheet(
-                          realtimeWeather: state.realtimeWeather!,
-                        );
-                      } else {
-                        return Container(
-                          height: 550.sp,
-                          color: Colors.red,
-                        );
-                      }
-                    },
-                  );
-                });
-          },
-          label: const Text('Get weather'),
-          icon: const Icon(Icons.directions_boat),
-        ),
+        // floatingActionButton: FloatingActionButton.extended(
+        //   onPressed: () async {
+        //     context
+        //         .read<RealtimeWeatherBloc>()
+        //         .add(FetchRealtimeWeatherEvent(middleOfTheMap!.asString()));
+        //     await showModalBottomSheet(
+        //         useSafeArea: true,
+        //         //barrierColor: Colors.blue,
+        //         isScrollControlled: true,
+        //         showDragHandle: true,
+        //         context: context,
+        //         builder: (context) {
+        //           return BlocBuilder<RealtimeWeatherBloc, RealtimeWeatherState>(
+        //             builder: (context, state) {
+        //               if (state is RealtimeWeatherLoading) {
+        //                 return Container();
+        //               } else if (state is RealtimeWeatherDone) {
+        //                 return WeatherModalSheet(
+        //                   realtimeWeather: state.realtimeWeather!,
+        //                 );
+        //               } else {
+        //                 return Container(
+        //                   height: 550.sp,
+        //                   color: Colors.red,
+        //                 );
+        //               }
+        //             },
+        //           );
+        //         });
+        //   },
+        //   label: const Text('Get weather'),
+        //   icon: const Icon(Icons.directions_boat),
+        // ),
       ),
     );
   }
