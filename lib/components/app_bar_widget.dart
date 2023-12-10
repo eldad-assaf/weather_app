8e453d5dd05d90a153a3ad233d51c0fd5f56c5a8
eldad assaf <eldadux@gmail.com>
2023-12-10 12:48:47 +0200

    OK

--- lib/components/app_bar_widget.dart
+++ lib/components/app_bar_widget.dart
@@ -4,10 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_app/components/alert_dialog_model.dart';
 import 'package:weather_app/components/reuseable_text.dart';
 import 'package:weather_app/components/text_style.dart';
+import 'package:weather_app/core/extensions/extension_methods.dart';
 import 'package:weather_app/features/device_position/presentation/bloc/device_position_bloc.dart';
 import 'package:weather_app/features/map_view/presentation/bloc/camera_position_bloc.dart';
 import 'package:weather_app/features/map_view/presentation/pages/home/map_view.dart';
@@ -32,13 +32,14 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
       title: ReusableText(
           text: 'Weather', style: appStyle(18, Colors.white, FontWeight.w300)),
       actions: [
-        IconButton(
-            color: Colors.red,
-            onPressed: () async {
-              SharedPreferences sf = await SharedPreferences.getInstance();
-              await sf.clear();
-            },
-            icon: const Icon(Icons.delete)),
+        //**FOR TESTING SHAREDPREFS DATA */
+        // IconButton(
+        //     color: Colors.red,
+        //     onPressed: () async {
+        //       SharedPreferences sf = await SharedPreferences.getInstance();
+        //       await sf.clear();
+        //     },
+        //     icon: const Icon(Icons.delete)),
         BlocListener<DevicePositionBloc, DevicePositionState>(
           listener: (context, state) {
             if (state is DevicePositionDone) {
